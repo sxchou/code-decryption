f30efdd22809e5731b3ea5533c10507547867f1c
sxchou <125382693+sxchou@users.noreply.github.com>
2023-04-28 14:43:37 +0800

    Update login_chaojiying.py

--- login_chaojiying.py
+++ login_chaojiying.py
@@ -9,7 +9,7 @@ driver.find_element(By.XPATH, '//*[@id="carousel-example-generic"]/div/div[1]/ar
 driver.find_element(By.XPATH, '/html/body/div[3]/div/div[3]/div[1]/form/p[1]/input').send_keys('超级鹰账号')
 driver.find_element(By.XPATH, '/html/body/div[3]/div/div[3]/div[1]/form/p[2]/input').send_keys('超级鹰密码')
 img = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[3]/div[1]/form/div/img').screenshot_as_png
-chaojiying = Chaojiying_Client('超级鹰账号', '超级鹰密码', '96001')
+chaojiying = Chaojiying_Client('超级鹰账号...', '超级鹰密码...', '96001')
 dic = chaojiying.PostPic(img, 1902)
 verify_code = dic['pic_str']
 driver.find_element(By.XPATH, '/html/body/div[3]/div/div[3]/div[1]/form/p[3]/input').send_keys(verify_code)
